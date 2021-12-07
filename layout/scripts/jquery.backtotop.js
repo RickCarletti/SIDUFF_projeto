48cd1dc428086f8efc6e680ed9a5ab53e147d5ab
Ricardo Carletti <ricardocarletti@id.uff.br>
2021-12-06 21:01:02 -0300

    Iniciando

---
+++ layout/scripts/jquery.backtotop.js
@@ -0,0 +1,17 @@
+/*
+Template Name: Chillaid
+Author: <a href="https://www.os-templates.com/">OS Templates</a>
+Author URI: https://www.os-templates.com/
+Copyright: OS-Templates.com
+Licence: Free to use under our free template licence terms
+Licence URI: https://www.os-templates.com/template-terms
+File: Back to Top JS
+*/
+
+jQuery(window).scroll(function () {
+    if (jQuery(window).scrollTop() > 150) {
+        jQuery("#backtotop").addClass("visible");
+    } else {
+        jQuery("#backtotop").removeClass("visible");
+    }
+});
