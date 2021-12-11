825a0994852b752a69f7eb8efe58f0becf346c99
LucaT42 <loliveira@ayesa.com>
2021-12-11 01:06:48 -0300

    final

--- layout/scripts/chart.js
+++ layout/scripts/chart.js
@@ -5,7 +5,7 @@ type: 'pie',
 data: {
 labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
 datasets: [{
-data: [300, 50, 100, 40, 120],
+data: [350, 50, 100, 40, 120],
 backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
 hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
 }]
